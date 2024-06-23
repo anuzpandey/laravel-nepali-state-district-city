2474c1b18cec84dfe9365adaee89a20a5e2f4d96
AnuzPandey <anuzbvbmaniac123@gmail.com>
2024-06-23 18:15:09 +0545

    :recycle: refac: Update Package Base Directory.

--- src/LaravelNepaliStateDistrictCityServiceProvider.php
+++ src/LaravelNepaliStateDistrictCityServiceProvider.php
@@ -10,16 +10,9 @@ class LaravelNepaliStateDistrictCityServiceProvider extends PackageServiceProvid
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-nepali-state-district-city')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-nepali-state-district-city_table')
-            ->hasCommand(LaravelNepaliStateDistrictCityCommand::class);
+            ->hasMigration('create_laravel-nepali-state-district-city_table');
     }
 }
